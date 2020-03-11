8f76c8211f0bfa1d14d90266bf07c246b3020101
JakMar17 <jakob.marusic17@gmail.com>
2020-03-11 22:47:34 +0100

    added map on home screen

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -11,10 +11,6 @@ export class MapComponent implements OnInit {
   constructor() { }
 
   @Input() casesByCountries: Country[];
-<<<<<<< HEAD
-=======
-  
->>>>>>> master
 
   type="GeoChart";
   columnNames = [
