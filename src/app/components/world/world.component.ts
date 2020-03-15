e1f6a87900b135c82aeaa3dd80cdc177f06d3dc6
JakMar17 <jakob.marusic17@gmail.com>
2020-03-15 11:17:33 +0100

    fixed map chart

--- src/app/components/world/world.component.ts
+++ src/app/components/world/world.component.ts
@@ -55,7 +55,7 @@ export class WorldComponent implements OnInit {
     
                 this.setPieChartTypes();
                 this.setChartClosedCases();
-    
+                this.mapChartSetData();
               });
 
           });
@@ -132,6 +132,60 @@ export class WorldComponent implements OnInit {
   private setChartClosedCases(): void {
     this.pieChartClosedCases.push(['Deaths', this.worldLatest.deaths, "#680114"]);
     this.pieChartClosedCases.push(['Recovered', this.worldLatest.recovered, "#86DB41"]);
+  }
+
+  public mapChartType: string = "GeoChart";
+  public mapChartColumnNames = [
+    [{type: 'string', role: 'data'}],
+    [{type: 'number', role: 'data'}]
+  ];
+  public mapChartData = [];
+  public mapChartOptions = {
+    colorAxis: {colors: [
+      '#F0FF82',
+      '#E7FF33',
+      '#FFEC33',
+      '#FFD433',
+      '#FFA721',
+      '#FF7821',
+      '#FF2121',
+      '#CA0707',
+      '#A90505'
+    ]},
+    legend: "none"
+  };
+
+  private mapChartSetData(): void {
+    this.casesByCountries.forEach(element => {
+      let temp = [];
+
+      switch(element.countryName) {
+        case "Mainland China":
+          temp.push("China");
+          break;
+        case "Russian Federation":
+          temp.push("Russia");
+          break;
+        case "UK":
+          temp.push("United Kingdom");
+          break;
+        case "Republic of Korea":
+          temp.push("South Korea");
+          break;
+        case "Iran (Islamic Republic of)":
+          temp.push("Iran");
+          break;
+        default:
+          temp.push(element.countryName);
+      }
+    
+      let v: number = 1024 * Math.log(element.sumOfConfirms) / 5;
+
+      let tempNumber = {f: element.sumOfConfirms, v: v};
+
+      temp.push(tempNumber);
+      this.mapChartData.push(temp);
+    });
   }
 
 }
